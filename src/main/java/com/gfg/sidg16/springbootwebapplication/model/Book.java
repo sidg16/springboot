2cffa1a752bf1a67e46e2bcd40ea464c50ea0c93
Siddhant Gupta <sid@Siddhants-MacBook-Air.local>
2019-06-09 15:41:17 +0530

    File add

---
+++ src/main/java/com/gfg/sidg16/springbootwebapplication/model/Book.java
@@ -0,0 +1,6 @@
+package com.gfg.sidg16.springbootwebapplication.model;
+
+public class Book {
+    private String author;
+
+}
---
+++ src/test/java/com/gfg/sidg16/springbootwebapplication/SpringbootwebapplicationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gfg.sidg16.springbootwebapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootwebapplicationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
